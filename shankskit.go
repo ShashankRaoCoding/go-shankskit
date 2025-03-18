0bbd5ba6379291698c2ac610fe7c7b41be3f967a
ShashankRao <158217706+ShashankRaoCoding@users.noreply.github.com>
2025-03-18 14:33:26 +0000

    Update shankskit.go

--- shankskit.go
+++ shankskit.go
@@ -9,7 +9,7 @@ import (
 ) 
 
 func Input(prompt string) string {
-	fmt.Print(prompt)
+	fmt.Print(prompt, "")
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	return input
