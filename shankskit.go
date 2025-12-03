d807453a9b9b92d99db2adbe664767b89ae421d7
ShashankRao <158217706+ShashankRaoCoding@users.noreply.github.com>
2025-12-03 17:07:13 +0000

    Implement StartServer function for HTTP server
    Added StartServer function to initialize and run an HTTP server with dynamic port allocation.

--- shankskit.go
+++ shankskit.go
@@ -130,3 +130,27 @@ func Respond(w http.ResponseWriter, filePath string, data interface{}) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
+
+func StartServer(routes map[string]http.HandlerFunc) string {
+
+	ln, _ := net.Listen("tcp", ":0")
+	port := fmt.Sprintf("%v", ln.Addr().(*net.TCPAddr).Port)
+	mux := http.NewServeMux()
+	for url, handler := range routes {
+		mux.HandleFunc(url, handler)
+	}
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	go func() {
+		if err := server.Serve(ln); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Error starting server:", err)
+		}
+	}()
+
+	log.Printf("Access the server at localhost:%s", port)
+
+	return port
+
+}
